9101764d570fa96dc12dee26597c501345ad1cc9
king_badhan <pankaj4utanda@gmail.com>
2024-09-12 15:38:24 +0530

    initialization

--- JS/1_basics/04.js
+++
@@ -1,8 +0,0 @@
-// let keyword 
-// declare variable with let keyword 
-
-let firstName = "Pankaj";
-firstName = "Loveleen";
-console.log(firstName);
-
-// block scope vs funtion scope (covered later)
--- JS/3_dom/107.js
+++
@@ -1,8 +0,0 @@
-// change text:  textContent & innerText
-
-const mainHeading = document.getElementById("main-heading");
-console.log(mainHeading.textContent);
-console.log(mainHeading.innerText);
-mainHeading.textContent = "This is something else";
-console.log(mainHeading.textContent);
-console.dir(mainHeading.innerText);
